eee79021eb748a45f7ac54f7de07ce85cbaef2bd
CodeVij <b.vijithreddy@gmail.com>
2015-07-07 23:07:33 -0700

    Update with comments

--- src/main/java/org/spark/workingsample/WordCountSpark.java
+++ src/main/java/org/spark/workingsample/WordCountSpark.java
@@ -9,12 +9,11 @@ import java.util.Arrays;
 
 import scala.Tuple2;
 
-@SuppressWarnings("serial")
 public final class WordCountSpark {
 	public static void main(String[] args) throws Exception {
 		if (args.length < 4) {
 			System.err
-					.println("Usage: WordCountSpark <master> <input> <output> <numOutputFiles>");
+					.println("Please use: WordCountSpark <master> <input file> <output file> <numOutputFiles>");
 			System.exit(1);
 		}
 
@@ -22,32 +21,33 @@ public final class WordCountSpark {
 		JavaSparkContext spark = new JavaSparkContext(args[0],
 				"Java Wordcount", System.getenv("SPARK_HOME"),
 				JavaSparkContext.jarOfClass(WordCountSpark.class));
-
+		// input file
 		JavaRDD<String> file = spark.textFile(args[1]);
-
+		// number of output files
 		Integer numOutputFiles = Integer.parseInt(args[3]);
 
-	
-		FlatMapFunction<String, String> wordMap=inputLine->{
-			return Arrays.asList(inputLine.toLowerCase().split("\\W+"));
+		// Flat map function using functional programming paradigm in java 8
+		FlatMapFunction<String, String> wordMap = inputPara -> {
+			return Arrays.asList(inputPara.toLowerCase().split("\\W+"));
 		};
-		
-		JavaRDD<String> words = file
-				.flatMap(wordMap);
-		
-		PairFunction<String, String, Integer> mapOnes=(String s)->{
+
+		JavaRDD<String> words = file.flatMap(wordMap);
+
+		// Map each word with Integer 1
+		PairFunction<String, String, Integer> mapOnes = s -> {
 			return new Tuple2<String, Integer>(s, 1);
 		};
 
 		JavaPairRDD<String, Integer> pairs = words.mapToPair(mapOnes);
-		
-		Function2<Integer, Integer, Integer>pairWords=(Integer a, Integer b)->{
+
+		// Add the integers with the same key
+		Function2<Integer, Integer, Integer> pairWords = (a, b) -> {
 			return a + b;
 		};
-
-		JavaPairRDD<String, Integer> counts = pairs.reduceByKey(
-				pairWords, numOutputFiles);
-
+		// Reduce by each word and generate the given number of output files
+		JavaPairRDD<String, Integer> counts = pairs.reduceByKey(pairWords,
+				numOutputFiles);
+		// Sort by key
 		counts.sortByKey(true).saveAsTextFile(args[2]);
 		System.exit(0);
 	}
